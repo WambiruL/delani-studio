3cbd95101723a093a047f0c7353d051ecca252ea
Lorraine <wambirulorraine@gmail.com>
2020-08-29 17:48:56 +0300

    Add the form validation

--- js/scripts.js
+++ js/scripts.js
@@ -10,16 +10,40 @@ $(document).ready(function(){
 
     $(".toggle-two").click(function(){
         $(".toggle-two img").toggle(1000);
-    })
+    });
     $(".toggle-two").click(function(){
         $(".hidden-two").css("text-align","center").css("font-size","15px").toggle(1000);
-    })
+    });
    
 
     $(".toggle-three").click(function(){
         $(".toggle-three img").toggle(1000);
-    })
+    });
     $(".toggle-three").click(function(){
         $(".hidden-three").css("text-align","center").css("font-size", "15px").toggle(1000);
+    });   
+
+    $("form#contact-us").submit(function(event){
+          event.PreventDefault();
+
     })
+   
 });
+
+//Business logic
+function validation(){
+    var name= document.getElementById("name").value;
+    var email= document.getElementById("email").value;
+    var message=document.getElementById("message").value;
+
+    if(name==""){
+        alert("Please enter valid name");
+    }else if(email.indexOf("@")== -1 || ""){
+        alert("Please enter valid email");
+    }else if(message==""){
+        alert("Please enter message");
+    }else{
+        alert( name + " we have received your message. Thank you for reaching out to us.");
+    };
+};
+console.log(validation());
